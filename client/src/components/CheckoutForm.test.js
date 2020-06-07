2717838e05c74174cec3841446062f426511423f
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-06-07 18:41:12 -0500

    trying to build 2nd test for form

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,27 @@
-import React from "react";
-import { render } from "@testing-library/react";
+import React, { version } from "react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", async () => {
+//Arrange
+const { getByText } = render(<CheckoutForm />);
 
-test("form shows success message on submit with form details", () => {});
+//Act
+const header = getByText(/checkout form/i);
+
+//Assert
+expect(header).toBeInTheDocument();
+});
+
+test("form shows success message on submit with form details", async () => {
+//Arrange
+const { getByTestId } = render(<CheckoutForm />);
+
+//Act
+const formMatch = getByTestId(/successMessage/);
+
+//Assert
+expect(formMatch).toBeInTheDocument();
+});
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -3,7 +3,7 @@ import { useState } from 'react'
 
 export const useForm = (initialValue) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState();
+  const [values, setValues] = useState(initialValue);
  
   const handleChanges = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
